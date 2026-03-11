6197742d4f37e9735cfb1efce81a64184d595717
ReyRodriP <reynaldorodriguez802@gmail.com>
2026-03-10 21:24:44 -0400

    Version final y errores al cerrar notificacion corregido

--- script.js
+++ script.js
@@ -4,78 +4,53 @@ const notificaciones = document.getElementById('toast');
 botones.addEventListener('click', (e) => {
     switch(e.target.className) {
      case 'btn-sucess':
-        let exitCode = 
-        `<div class="toast sucess tiempo">
-            <div class="toast-icon">
-                <img src="icons/check-circle.svg" alt="advertencia">
-            </div>
-            <div class="toast-text">
-                <h3>Exito</h3>
-                <p>Todo correcto</p>
-            </div>
-            <div class="toast-button">
-                <button id="closeSucess"><img src="icons/x.svg" alt=""></button>
-            </div>
-        </div>`
-      
-        notificaciones.insertAdjacentHTML('beforeend', exitCode)
-        cierreAutomatico(notificaciones.lastElementChild);
+       addNotification('sucess', 'check-circle.svg', 'Exito', 'Todo correcto')
      break;
 
      case 'btn-warning':
-        let warningCode = `
-        <div class="toast warning tiempo">
-            <div class="toast-icon">
-                <img src="icons/alert-triangle.svg" alt="advertencia">
-            </div>
-            <div class="toast-text">
-                <h3>Advertencia</h3>
-                <p>Hay ligeros detalles</p>
-            </div>
-            <div class="toast-button">
-                <button id="closeWarning"><img src="icons/x.svg" alt=""></button>
-            </div>
-        </div>`
-
-        notificaciones.insertAdjacentHTML('beforeend', warningCode)
-        cierreAutomatico(notificaciones.lastElementChild);
+      addNotification('warning', 'alert-triangle.svg', 'Advertencia', 'Hay ligeros detalles')
      break;
 
      case 'btn-error':
-         let errorCode = `
-        <div class="toast error tiempo">
-            <div class="toast-icon">
-                <img src="icons/x-circle.svg" alt="advertencia">
-            </div>
-            <div class="toast-text">
-                <h3>Error</h3>
-                <p>No se pudo completar la accion</p>
-            </div>
-            <div class="toast-button">
-                <button id="closeError"><img src="icons/x.svg" alt=""></button>
-            </div>
-        </div>`
-
-        notificaciones.insertAdjacentHTML('beforeend', errorCode) 
-        cierreAutomatico(notificaciones.lastElementChild);
+      addNotification('error', 'x-circle.svg', 'Error', 'No se pudo completar la accion')
      break;
     }  
 });
 
+//Funcion para crear las notificaciones 
+function addNotification(type, image, title, content) {
+   const Code = `
+   <div class="toast ${type} tiempo">
+      <div class="toast-icon">
+            <img src="icons/${image}" alt="Notificacion">
+      </div>
+      <div class="toast-text">
+            <h3>${title}</h3>
+            <p>${content}</p>
+      </div>
+         <div class="toast-button">
+            <button id="closeSucess"><img src="icons/x.svg" alt=""></button>
+      </div>
+   </div>
+   `
+   notificaciones.insertAdjacentHTML('beforeend', Code) 
+   cierreAutomatico(notificaciones.lastElementChild);
+}
+
+//Cierre manual por boton
 notificaciones.addEventListener('click', (e) => {
    const botonCerrar = e.target.closest('button')
    if(!botonCerrar) return 
    
    const toast = botonCerrar.closest('.toast')
-   toast.classList.add('.cierre')
+   toast.classList.add('cierre')
 
    setTimeout(() => {
       toast.remove();
-   }, 300)
+   }, 400)
 })
 
 //Funcion para manejar el cierre - Pendiente
-
 function cierreAutomatico(toast) {
    setTimeout(() => {
          toast.classList.add('cierre')
